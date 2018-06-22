18aa2ff398955bf6090698995fb996c7ff5a784b
AitorSDL <32193889+AitorSDL@users.noreply.github.com>
2018-06-22 15:44:30 +0100

    Add files via upload

--- nn_code/nn_mnist.py
+++ nn_code/nn_mnist.py
@@ -1,5 +1,5 @@
 import gzip
-import cPickle
+import pickle as cPickle
 
 import tensorflow as tf
 import numpy as np
@@ -22,28 +22,36 @@ def one_hot(x, n):
 
 
 f = gzip.open('mnist.pkl.gz', 'rb')
-train_set, valid_set, test_set = cPickle.load(f)
+# 50000, 10000, 10000 len
+train_set, valid_set, test_set = cPickle.load(f, encoding='iso-8859-1')
 f.close()
 
 train_x, train_y = train_set
-
+train_y = one_hot(train_y, 10)
+valid_x, valid_y = valid_set
+valid_y = one_hot(valid_y, 10)
+test_x, test_y = test_set
+test_y = one_hot(test_y, 10)
 
 # ---------------- Visualizing some element of the MNIST dataset --------------
 
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
-plt.imshow(train_x[57].reshape((28, 28)), cmap=cm.Greys_r)
-plt.show()  # Let's see a sample
-print train_y[57]
+# plt.imshow(train_x[56].reshape((28, 28)), cmap=cm.Greys_r)
+# plt.show()  # Let's see a sample
+# print(train_y[56])
 
-x = tf.placeholder("float", [None, 784])  # samples
-y_ = tf.placeholder("float", [None, 10])  # labels
+# TODO: the neural net!!
+x = tf.placeholder("float", [None, 784])  # samples 28x28 pixels
+y_ = tf.placeholder("float", [None, 10])  # labels  numbers
 
-W1 = tf.Variable(np.float32(np.random.rand(784, 14)) * 0.1)
-b1 = tf.Variable(np.float32(np.random.rand(14)) * 0.1)
+# 784 entries, 5 neurons
+W1 = tf.Variable(np.float32(np.random.rand(784, 20)) * 0.1)
+b1 = tf.Variable(np.float32(np.random.rand(20)) * 0.1)
 
-W2 = tf.Variable(np.float32(np.random.rand(14, 10)) * 0.1)
+# 5 entries, 10 neurons
+W2 = tf.Variable(np.float32(np.random.rand(20, 10)) * 0.1)
 b2 = tf.Variable(np.float32(np.random.rand(10)) * 0.1)
 
 h = tf.nn.sigmoid(tf.matmul(x, W1) + b1)
@@ -54,21 +62,62 @@ loss = tf.reduce_sum(tf.square(y_ - y))
 
 train = tf.train.GradientDescentOptimizer(0.01).minimize(loss)  # learning rate: 0.01
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
 sess = tf.Session()
 sess.run(init)
 
+print("----------------------")
+print("   Start training...  ")
+print("----------------------")
+
 batch_size = 20
 
-for epoch in xrange(100):
-    for jj in xrange(len(x_data) / batch_size):
-        batch_xs = x_data[jj * batch_size: jj * batch_size + batch_size]
-        batch_ys = y_data[jj * batch_size: jj * batch_size + batch_size]
+valid_error_values = []
+training_error_values = []
+
+current_error = 1.
+last_error = 0.
+epoch = 0
+
+while (abs(current_error - last_error)) > .001:
+    epoch += 1
+    for jj in range(len(train_x) // batch_size):  # Este bucle se ejecuta 5
+        batch_xs = train_x[jj * batch_size: jj * batch_size + batch_size]
+        batch_ys = train_y[jj * batch_size: jj * batch_size + batch_size]
         sess.run(train, feed_dict={x: batch_xs, y_: batch_ys})
 
-    print "Epoch #:", epoch, "Error: ", sess.run(loss, feed_dict={x: batch_xs, y_: batch_ys})
-    result = sess.run(y, feed_dict={x: batch_xs})
-    for b, r in zip(batch_ys, result):
-        print b, "-->", r
-    print "----------------------------------------------------------------------------------"
+    # validation
+    current_error = sess.run(loss, feed_dict={x: batch_xs, y_: batch_ys})
+    training_error_values.append(current_error)
+    if epoch > 1:
+        last_error = training_error_values[-2]
+    print('Epoch #:', epoch, 'Training error:\t', current_error)
+
+    validation_error = sess.run(loss, feed_dict={x: valid_x, y_: valid_y}) / len(valid_y) * 100
+    valid_error_values.append(validation_error)
+    print('\t\t  ', 'Validation error:', validation_error)
+
+print("------------------")
+print("   Start test...  ")
+print("------------------")
+
+good = 0.
+result = sess.run(y, feed_dict={x: test_x})
+for b, r in zip(test_y, result):
+    # print(b, "-->", r)
+    # print(np.argmax(b), "-->", np.argmax(r))
+    if (np.argmax(b) == np.argmax(r)):
+        good += 1
+print('------------------------------------------------')
+print('Test success: %.2f%%' % (good / len(result) * 100))
+
+plt.title('Training vs Validation')
+plt.ylabel('Error')
+plt.xlabel('Epoch')
+training_plot, = plt.plot(training_error_values)
+validation_plot, = plt.plot(valid_error_values)
+plt.legend(handles=[training_plot, validation_plot],
+           labels=['Training error', 'Validation error'])
+# plt.savefig('mnist_graph.png')
+plt.show()
